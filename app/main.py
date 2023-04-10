13f5d54157d1849ef88123a0bba39d71e1d64d9c
YT <y.taira@apptime.co.jp>
2023-04-11 05:05:05 +0900

    test

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from gyazo.api import Api
 from gyazo.error import GyazoError
 from gyazo.image import Image, ImageList
 from time import sleep
+from app.libs import trim
 # os
 import os
 app = FastAPI()
@@ -14,15 +15,32 @@ client = Api(access_token=os.environ['GYAZO_ACCESS_TOKEN'])
 
 @app.post("/upload/")
 async def upload(file: UploadFile = File(...)):
-    # HEICをJPGに変換
+    # cv2で画像を読み込み
+    input_file = np.frombuffer(await file.read(), np.uint8)
+    input_image = cv2.imdecode(input_file, cv2.IMREAD_COLOR)
+    # グレースケールに変換
+    gray_img = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
 
-    image = await file.read()
-    # グレースケール変換
-    image = cv2.imdecode(np.frombuffer(image, np.uint8), 0)
+    # 適応的閾値処理
+    binary_img = cv2.adaptiveThreshold(
+        compressed_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 255, 2
+    )
+    # ガウシアンフィルタで画像の平滑化
+    size = (3, 3)
+    blur = cv2.GaussianBlur(binary_img, size, 0)
+    # 大津の手法を使った画像の２値化
+    _, th = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # # 白黒反転
+    # th = cv2.bitwise_not(th)
+    # # 大津の手法を使った画像の２値化
+    # _, th = cv2.threshold(th, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # # 白黒反転で元に戻す
+    # th = cv2.bitwise_not(th)
+
     # # サイズを500kbに圧縮
-    image = compress_image_to_500kb(image)
-    # # ファイルの形式を戻す
-    image = cv2.imencode(".jpg", image)[1].tobytes()
+    image = compress_image_to_500kb(blur)
+    # ファイルの形式を戻す
+    image = cv2.imencode(".jpg", blur)[1].tobytes()
     
 
     # imgをgyazoにアップロード
@@ -30,9 +48,14 @@ async def upload(file: UploadFile = File(...)):
     sleep(8)
     test = client.get_image(image_id=img.image_id)
     # test.ocrがない場合はリトライ
+    count = 0
     while test.ocr is None:
         sleep(4)
         test = client.get_image(image_id=img.image_id)
+        # 3回リトライしてもダメならエラーを返す
+        count += 1
+        if count >= 3:
+            return {"error": "OCR取得に失敗しました"}
 
     # imgのocrを取得
     print(test.ocr)
